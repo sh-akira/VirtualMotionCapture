e2f74651423b018923bf72b793a0abf675730bea
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2019-10-13 00:57:42 +0900

    Fix root dosen't move when Full Body Tracking.

--- Assets/ExternalSender/ExternalSender.cs
+++ Assets/ExternalSender/ExternalSender.cs
@@ -3,48 +3,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RootMotion.FinalIK;
 
 public class ExternalSender : MonoBehaviour {
-    uOSC.uOscClient uClient;
-    GameObject CurrentModel;
-    ControlWPFWindow window;
-   
+    uOSC.uOscClient uClient = null;
+    GameObject CurrentModel = null;
+    ControlWPFWindow window = null;
+    Animator animator = null;
+    VRIK vrik = null;
 
-    // Use this for initialization
     void Start () {
         uClient = GetComponent<uOSC.uOscClient>();
         window = GameObject.Find("ControlWPFWindow").GetComponent<ControlWPFWindow>();
+
+        window.ModelLoadedAction += (GameObject CurrentModel) =>
+        {
+            this.CurrentModel = CurrentModel;
+            animator = CurrentModel.GetComponent<Animator>();
+            vrik = CurrentModel.GetComponent<VRIK>();
+        };
     }
-	
+
 	// Update is called once per frame
 	void Update () {
-
-        Animator animator = null;
-
-        CurrentModel = window.GetCurrentModel();
-        if (CurrentModel != null)
+        if (CurrentModel != null && animator != null && uClient != null)
         {
-            animator = CurrentModel.GetComponent<Animator>();
-        }
+            //Root
+            if (vrik == null)
+            {
+                vrik = CurrentModel.GetComponent<VRIK>();
+                Debug.Log("ExternalSender: VRIK Updated");
+            }
 
-        if (animator != null)
-        {
+            if (vrik != null)
+            {
+                var RootTransform = vrik.references.root;
+                if (RootTransform != null)
+                {
+                    uClient.Send("/VMC/ExternalSender/Root/Transform",
+                        "root",
+                        RootTransform.position.x, RootTransform.position.y, RootTransform.position.z,
+                        RootTransform.rotation.x, RootTransform.rotation.y, RootTransform.rotation.z, RootTransform.rotation.w);
+                }
+            }
+
+            //Bones
             foreach (HumanBodyBones bone in Enum.GetValues(typeof(HumanBodyBones)))
             {
                 var Transform = animator.GetBoneTransform(bone);
                 if (Transform != null)
                 {
-                    uClient.Send("/VMC/ExternalSender/Bone/Transform", bone.ToString(), Transform.localPosition.x, Transform.localPosition.y, Transform.localPosition.z, Transform.localRotation.x, Transform.localRotation.y, Transform.localRotation.z, Transform.localRotation.w);
+                    uClient.Send("/VMC/ExternalSender/Bone/Transform", 
+                        bone.ToString(), 
+                        Transform.localPosition.x, Transform.localPosition.y, Transform.localPosition.z, 
+                        Transform.localRotation.x, Transform.localRotation.y, Transform.localRotation.z, Transform.localRotation.w);
                 }
             }
             uClient.Send("/VMC/ExternalSender/Available", 1);
-            uClient.Send("/VMC/ExternalSender/Time", Time.time);
         }
         else
         {
             uClient.Send("/VMC/ExternalSender/Available", 0);
-            uClient.Send("/VMC/ExternalSender/Time", Time.time);
         }
+        uClient.Send("/VMC/ExternalSender/Time", Time.time);
     }
 }
 
