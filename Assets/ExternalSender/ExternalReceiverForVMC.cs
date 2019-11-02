4a9ea3b7ed9f2df55efb45cc78a3cdd187be4701
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2019-11-03 01:21:16 +0900

    add comments, add frame setting

--- Assets/ExternalSender/ExternalReceiverForVMC.cs
+++ Assets/ExternalSender/ExternalReceiverForVMC.cs
@@ -6,11 +6,13 @@ using Valve.VR;
 
 [RequireComponent(typeof(uOSC.uOscServer))]
 public class ExternalReceiverForVMC : MonoBehaviour {
-    Vector3 pos;
-    Quaternion rot;
+    public ExternalSender externalSender;
 
     public SortedDictionary<string, SteamVR_Utils.RigidTransform> virtualController = new SortedDictionary<string, SteamVR_Utils.RigidTransform>();
     public SortedDictionary<string, SteamVR_Utils.RigidTransform> virtualTracker = new SortedDictionary<string, SteamVR_Utils.RigidTransform>();
+
+    Vector3 pos;
+    Quaternion rot;
 
     void Start () {
         var server = GetComponent<uOSC.uOscServer>();
@@ -22,6 +24,7 @@ public class ExternalReceiverForVMC : MonoBehaviour {
         //有効なとき以外処理しない
         if (this.isActiveAndEnabled)
         {
+            //仮想コントローラー V2.3
             if (message.address == "/VMC/Ext/Con/Pos"
                 && (message.values[0] is string)
                 && (message.values[1] is float)
@@ -45,9 +48,8 @@ public class ExternalReceiverForVMC : MonoBehaviour {
                     virtualController.Add(serial, rigidTransform);
                 }
             }
-
-
-            if ((message.address == "/VMC/Ext/Hmd/Pos"
+            //仮想トラッカー V2.3
+            else if ((message.address == "/VMC/Ext/Hmd/Pos"
                 || message.address == "/VMC/Ext/Tra/Pos")
                 && (message.values[0] is string)
                 && (message.values[1] is float)
@@ -71,9 +73,25 @@ public class ExternalReceiverForVMC : MonoBehaviour {
                     virtualTracker.Add(serial, rigidTransform);
                 }
             }
+            //フレーム設定 V2.3
+            else if (message.address == "/VMC/Ext/Set/Period"
+                && (message.values[0] is int)
+                && (message.values[1] is int)
+                && (message.values[2] is int)
+                && (message.values[3] is int)
+                && (message.values[4] is int)
+                && (message.values[5] is int)
+            )
+            {
+                externalSender.periodStatus = (int)message.values[0];
+                externalSender.periodRoot = (int)message.values[1];
+                externalSender.periodBone = (int)message.values[2];
+                externalSender.periodBlendShape = (int)message.values[3];
+                externalSender.periodCamera = (int)message.values[4];
+                externalSender.periodDevices = (int)message.values[5];
+            }
         }
     }
-
     SteamVR_Utils.RigidTransform SetTransform(ref Vector3 pos, ref Quaternion rot,ref uOSC.Message message) {
         pos.x = (float)message.values[1];
         pos.y = (float)message.values[2];
