b96f246120aa2ff795cabd250d1d7da00a36dab7
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-09-17 22:53:22 +0900

    ValueBinding

--- ControlWindowWPF/ControlWindowWPF/GraphicsOptionWindow.xaml.cs
+++ ControlWindowWPF/ControlWindowWPF/GraphicsOptionWindow.xaml.cs
@@ -20,8 +20,6 @@ namespace VirtualMotionCaptureControlPanel
     /// </summary>
     public partial class GraphicsOptionWindow : Window
     {
-        PipeCommands.SetPostProcessing postProcessingValue = new PipeCommands.SetPostProcessing();
-
         public GraphicsOptionWindow()
         {
             InitializeComponent();
@@ -44,20 +42,75 @@ namespace VirtualMotionCaptureControlPanel
             if (e.CommandType == typeof(PipeCommands.SetPostProcessing))
             {
                 var d = (PipeCommands.SetPostProcessing)e.Data;
-                postProcessingValue = d;
-                ValueApply();
+                ValueApply(d);
             }
         }
 
         private async void ValueChanged()
         {
             //セットされたpostProcessingValueを送信する
-            await Globals.Client.SendCommandAsync(postProcessingValue);
+            await Globals.Client.SendCommandAsync(new PipeCommands.SetPostProcessing { 
+                AntiAliasing_Enable = AntiAliasing_Enable_CheckBox.IsChecked.Value,
+                
+                Bloom_Enable = Bloom_Enable_CheckBox.IsChecked.Value,
+                Bloom_Intensity = (float)Bloom_Intensity_Slider.Value,
+
+                DoF_Enable = DoF_Enable_CheckBox.IsChecked.Value,
+                DoF_FocusDistance = (float)DoF_FocusDistance_Slider.Value,
+                DoF_Aperture = (float)DoF_Aperture_Slider.Value,
+                DoF_FocusLength = (float)DoF_FocusLength_Slider.Value,
+                DoF_MaxBlurSize = (float)DoF_MaxBlurSize_Slider.Value,
+
+                CG_Enable = CG_Enable_CheckBox.IsChecked.Value,
+                CG_Temperature = (float)CG_Temperature_Slider.Value,
+                CG_Saturation = (float)CG_Saturation_Slider.Value,
+                CG_Contrast = (float)CG_Contrast_Slider.Value,
+
+                Vignette_Enable = Vignette_Enable_CheckBox.IsChecked.Value,
+                Vignette_Intensity = (float)Vignette_Intensity_Slider.Value,
+                Vignette_Smoothness = (float)Vignette_Smoothness_Slider.Value,
+                Vignette_Rounded = (float)Vignette_Rounded_Slider.Value,
+
+                CA_Enable = CA_Enable_CheckBox.IsChecked.Value,
+                CA_Intensity = (float)CA_Intensity_Slider.Value
+            });
         }
-        private async void ValueApply()
+        private async void ValueApply(PipeCommands.SetPostProcessing d)
         {
             //postProcessingValueを画面に反映する
+            AntiAliasing_Enable_CheckBox.IsChecked = d.AntiAliasing_Enable;
+
+            Bloom_Enable_CheckBox.IsChecked = d.Bloom_Enable;
+            Bloom_Intensity_Slider.Value = d.Bloom_Intensity;
+
+            DoF_Enable_CheckBox.IsChecked = d.DoF_Enable;
+            DoF_FocusDistance_Slider.Value = d.DoF_FocusDistance;
+            DoF_Aperture_Slider.Value = d.DoF_Aperture;
+            DoF_FocusLength_Slider.Value = d.DoF_FocusLength;
+            DoF_MaxBlurSize_Slider.Value = d.DoF_MaxBlurSize;
+
+            CG_Enable_CheckBox.IsChecked = d.CG_Enable;
+            CG_Temperature_Slider.Value = d.CG_Temperature;
+            CG_Saturation_Slider.Value = d.CG_Saturation;
+            CG_Contrast_Slider.Value = d.CG_Contrast;
+
+            Vignette_Enable_CheckBox.IsChecked = d.Vignette_Enable;
+            Vignette_Intensity_Slider.Value = d.Vignette_Intensity;
+            Vignette_Smoothness_Slider.Value = d.Vignette_Smoothness;
+            Vignette_Rounded_Slider.Value = d.Vignette_Rounded;
+
+            CA_Enable_CheckBox.IsChecked = d.CA_Enable;
+            CA_Intensity_Slider.Value = d.CA_Intensity;
         }
 
+        private void SliverValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ValueChanged();
+        }
+
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+            ValueChanged();
+        }
     }
 }
