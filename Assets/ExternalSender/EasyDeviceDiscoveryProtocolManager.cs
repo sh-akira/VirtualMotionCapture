d0f4f1ad40d24b80b476e4ca75f17f3d30c73595
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-11-20 01:06:24 +0900

    Add auto device search.

---
+++ Assets/ExternalSender/EasyDeviceDiscoveryProtocolManager.cs
@@ -0,0 +1,40 @@
+﻿//gpsnmeajp
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyDeviceDiscoveryProtocolClient;
+
+public class EasyDeviceDiscoveryProtocolManager : MonoBehaviour
+{
+    public ExternalReceiverForVMC externalReceiver;
+    Requester requester;
+
+    public float time = 0;
+    public bool found = false;
+
+    void Start()
+    {
+        requester = gameObject.AddComponent<Requester>();
+        requester.deivceName = "Virtual Motion Capture";
+    }
+
+    void Update()
+    {
+        if (externalReceiver != null)
+        {
+            time += Time.deltaTime;
+            if (time > 5.0)
+            {
+                if (!found)
+                {
+                    requester.servicePort = externalReceiver.receivePort;
+                    requester.StartDiscover(() => {
+                        found = true;
+                    });
+                }
+                time = 0;
+            }
+        }
+    }
+}
