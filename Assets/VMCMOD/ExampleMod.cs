72e3bbe6017657e5c01d8f95554f492b236074bc
sh_akira <akira.satoh.sh@gmail.com>
2021-06-29 04:43:27 +0900

    Update example mod

--- Assets/VMCMOD/ExampleMod.cs
+++ Assets/VMCMOD/ExampleMod.cs
@@ -1,32 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using VMCMod;
 
 [VMCPlugin(
-    Name: "ExampleMOD", 
-    Version:"1.0.0", 
-    Author:"sh_akira", 
-    Description:"サンプルMOD",
-    AuthorURL:"https://twitter.com/sh_akira",
-    PluginURL:"https://vmc.info/")]
-public class ExampleMOD : MonoBehaviour
+    Name: "ExampleMod",
+    Version: "1.0.0",
+    Author: "sh_akira",
+    Description: "サンプルModです。右手に球体を取り付けます。Settingボタンで色をランダムに変更します。",
+    AuthorURL: "https://twitter.com/sh_akira",
+    PluginURL: "http://mod.vmc.info/")]
+public class ExampleMod : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform rightHandTransform;
+    private GameObject sphere;
+    private Material sphereMaterial;
+
+    private void Awake()
     {
-        
+        VMCEvents.OnModelLoaded += OnModelLoaded;
+        VMCEvents.OnCameraChanged += OnCameraChanged;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Example Mod started.");
+
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        sphereMaterial = sphere.GetComponent<Renderer>().material;
+    }
+
     void Update()
     {
-        
+        if (rightHandTransform != null)
+        {
+            sphere.transform.position = rightHandTransform.position;
+        }
     }
 
     [OnSetting]
     public void OnSetting()
     {
+        if (sphereMaterial != null)
+        {
+            sphereMaterial.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
 
+    private void OnModelLoaded(GameObject currentModel)
+    {
+        if (currentModel == null) return;
+
+        var animator = currentModel.GetComponent<Animator>();
+        rightHandTransform = animator.GetBoneTransform(HumanBodyBones.RightMiddleProximal); //右手中指のボーン
+    }
+
+    private void OnCameraChanged(Camera currentCamera)
+    {
+        //カメラ切り替え時に現在のカメラを取得できます
     }
 }
