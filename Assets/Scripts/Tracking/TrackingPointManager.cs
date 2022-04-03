e58a9f3f5cd28cb5785bacf34344f501c9ffb39b
sh_akira <akira.satoh.sh@gmail.com>
2022-04-04 02:04:40 +0900

    Add TrackingPointManager

---
+++ Assets/Scripts/Tracking/TrackingPointManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VMC
+{
+    public class TrackingPointManager : MonoBehaviour
+    {
+        public static TrackingPointManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private Dictionary<string, TrackingPoint> TrackingPoints = new Dictionary<string, TrackingPoint>();
+
+        public TrackingPoint ApplyPoint(string name, Vector3 position, Quaternion rotation)
+        {
+            //ignore "LIV Virtual Camera"
+
+            if (TrackingPoints.TryGetValue(name, out var trackingPoint) == false)
+            {
+                trackingPoint = new TrackingPoint(name);
+                TrackingPoints[name] = trackingPoint;
+                
+            }
+            trackingPoint.TargetTransform.localPosition = position;
+            trackingPoint.TargetTransform.localRotation = rotation;
+
+            return trackingPoint;
+        }
+
+        public TrackingPoint GetTrackingPoint(string name)
+        {
+            TrackingPoints.TryGetValue(name, out var trackingPoint);
+            return trackingPoint;
+        }
+
+        public Transform GetTransform(string name)
+        {
+            return GetTrackingPoint(name)?.TargetTransform;
+        }
+    }
+
+
+    public class TrackingPoint
+    {
+        public string Name { get; set; }
+        public Transform TargetTransform { get; set; }
+
+        public TrackingPoint(string name) => Name = name;
+    }
+
+}
