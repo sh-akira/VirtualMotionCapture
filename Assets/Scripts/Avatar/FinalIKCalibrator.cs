e99d4e539ed0c483cf89d22e5d0869feb43eb7c1
sh_akira <akira.satoh.sh@gmail.com>
2022-11-11 09:23:06 +0900

    頭がHMDの場合腰位置算出にHMDを使用

--- Assets/Scripts/Avatar/FinalIKCalibrator.cs
+++ Assets/Scripts/Avatar/FinalIKCalibrator.cs
@@ -211,6 +211,11 @@ namespace VMC
             // リアル手と手の中心から少し後ろに下げた位置
             var scaledCenterPosition = Vector3.Lerp(leftHandTargetTransform.position, rightHandTargetTransform.position, 0.5f) - hmdForwardAngle.normalized * (realHeight * wscale * 0.072f);
 
+            //頭がHMDの場合
+            if (headTarget.DeviceClass == ETrackedDeviceClass.HMD)
+            {
+                scaledCenterPosition = headTarget.TargetTransform.position - hmdForwardAngle.normalized * (realHeight * wscale * 0.063f);
+            }
             //身長から腰の位置を算出する
             // B12 臍高 965.7
             // B1 身長 1654.7
