73918166ff52edbdf1e0cf030e28be2900b4edb7
sh_akira <akira.satoh.sh@gmail.com>
2022-11-11 09:24:14 +0900

    背骨調整サンプル

---
+++ Assets/VRIKSpineOffset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class VRIKSpineOffset : MonoBehaviour
+{
+    public VRIK ik;
+    public Vector3 spineFix;
+
+    private void OnEnable()
+    {
+        ik.solver.OnPostUpdate += AfterVRIK;
+    }
+
+    private void OnDisable()
+    {
+        ik.solver.OnPostUpdate -= AfterVRIK;
+    }
+
+    private void AfterVRIK()
+    {
+        Vector3 headPos = ik.references.head.position;
+        Quaternion headRot = ik.references.head.rotation;
+
+        ik.references.spine.localRotation *= Quaternion.Euler(spineFix);
+        ik.references.chest.localRotation *= Quaternion.Euler(-spineFix);
+
+        ik.references.chest.rotation = Quaternion.FromToRotation(ik.references.head.position - ik.references.chest.position, headPos - ik.references.chest.position) * ik.references.chest.rotation;
+        ik.references.head.rotation = headRot;
+    }
+}
