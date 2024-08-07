2572b3a737dbcc3a9b610e51fd4aae0d9ea1d9c2
sh_akira <akira.satoh.sh@gmail.com>
2024-08-07 12:07:20 +0900

    [docs] add whitelist

--- docs/redirect/redirect.js
+++ docs/redirect/redirect.js
@@ -1,2 +1,12 @@
-const r = new URLSearchParams(window.location.search).get("r")
-if(r) window.location.replace(r)
+const allowedURLs = [
+    'ms-settings:*'
+];
+
+const r = new URLSearchParams(window.location.search).get('r');
+
+const isAllowed = allowedURLs.some((url) => {
+    const regex = new RegExp('^' + url.replace(/\*/g, '.*') + '$');
+    return regex.test(r);
+});
+
+if(r && isAllowed) window.location.replace(r);
